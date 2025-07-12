38069bb921d13b2d0348a6acdcc40d00f58b2b7a
rajapaksha320 <rajapakshagedara2001@gmail.com>
2025-07-12 22:33:50 +0530

    updated

--- src/pages/checkoutPage/AddressModal.jsx
+++ src/pages/checkoutPage/AddressModal.jsx
@@ -275,7 +275,7 @@ const AddressModal = ({ isOpen, onClose, onSave, editingAddress = null }) => {
           </div>
 
           {/* Company (Optional) */}
-          <div>
+          {/* <div>
             <label className="block text-sm font-medium text-gray-700 mb-2">
               Company (Optional)
             </label>
@@ -286,7 +286,7 @@ const AddressModal = ({ isOpen, onClose, onSave, editingAddress = null }) => {
               className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition-colors duration-200"
               placeholder="Enter company name"
             />
-          </div>
+          </div> */}
 
           {/* Address */}
           <div>
